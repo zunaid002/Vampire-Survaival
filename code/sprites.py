ec91300aa8ff68f6c6711bbc7e319c6339881984
Abdullah Al Zunaid <zunaidsefat@gmail.com>
2025-10-30 19:01:32 +0600

    enemy is not just one frame any more  animation is added with all the frames

--- code/sprites.py
+++ code/sprites.py
@@ -79,6 +79,15 @@ class Enemy(pygame.sprite.Sprite):
         self.collision = collision_sprite
         self.direction = pygame.math.Vector2()
         self.speed = 350
+
+    def move(self, dt):
+        pass
+
+    def animate(self, dt):
+        self.frame_index += self.animation_speed * dt
+        self.image = self.frames[int(self.frame_index) % len(self.frames)]
+
     
     def update(self, dt):
-        pass
+        self.move(dt)
+        self.animate(dt)
